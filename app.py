f2e6c939c51328be615f8554c93867d961ea699f
Rana Ahmed <116879537+rana412ahmed@users.noreply.github.com>
2026-08-26 00:54:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ if prompt := st.chat_input("اكتبي سؤالك هنا..."):
                 for msg in st.session_state.messages:
                     messages.append({"role": msg["role"], "content": msg["content"]})
                 response = client.chat.completions.create(
-                    model="llama-3.3-70b-versatile",
+                    # model="llama-3.3-70b-versatile",
+                    model="openai/gpt-oss-120b",
                     messages=messages,
                     max_tokens=500,
                     temperature=0.7
